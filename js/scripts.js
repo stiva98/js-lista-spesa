7025cb32e35881d6096517a60107b4d9790fba5f
stiva98 <samustiva9810@gmail.com>
2023-06-19 18:25:50 +0200

    ADD element li e terminato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,19 @@ const listOfShopping = [
     'Carne',
 ];
 
+const list = document.querySelector('.my-list')
 let i = 0;
 //console.log(listOfShopping)
 
 while (i < listOfShopping.length) {
-    console.log(i, listOfShopping[i])
+    //console.log(i, listOfShopping[i])
+
+    const newLi = document.createElement('li')
+    list.innerHTML += `
+        <li>
+            ${listOfShopping[i]}
+        </li>
+    `;
+    //console.log(i, listOfShopping[i])
     i++;
 }
